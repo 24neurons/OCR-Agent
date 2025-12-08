6aa2520b3e506c5bac1af35149a171e4a38788c9
ggcaochieu602 <caochihieu602@gmail.com>
2025-12-08 23:39:15 +0700

    cap nhat code moi

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
+#include <flutter_doc_scanner/flutter_doc_scanner_plugin_c_api.h>
 #include <speech_to_text_windows/speech_to_text_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FlutterDocScannerPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterDocScannerPluginCApi"));
   SpeechToTextWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SpeechToTextWindows"));
 }
